34ddede746dee941108fc8e6ddfacaf0ded91ecf
Jacek Kuśnierz <jaku@grapeup.com>
2021-08-25 16:32:10 +0200

    Initial commit for AWS

---
+++ bindings/experimental/distrdf/python/DistRDF/Backends/AWS/__init__.py
@@ -0,0 +1,21 @@
+## @author Vincenzo Eduardo Padulano
+#  @author Enric Tejedor
+#  @date 2021-02
+
+################################################################################
+# Copyright (C) 1995-2021, Rene Brun and Fons Rademakers.                      #
+# All rights reserved.                                                         #
+#                                                                              #
+# For the licensing terms see $ROOTSYS/LICENSE.                                #
+# For the list of contributors see $ROOTSYS/README/CREDITS.                    #
+################################################################################
+
+def RDataFrame(*args, **kwargs):
+    """
+    Create an RDataFrame object that can run computations on a Spark cluster.
+    """
+
+    from DistRDF.Backends.AWS import Backend
+    aws = Backend.AWSBackend()
+
+    return aws.make_dataframe(*args, **kwargs)
